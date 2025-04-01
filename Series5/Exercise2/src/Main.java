abccabe6737e800c5e36aa6e3656e41b97249917
Maurane Venegas <maurane.venegas@edu.hefr.ch>
2025-04-01 08:58:51 +0200

    Go back to Main.java

--- Series5/Exercise2/src/Main.java
+++ Series5/Exercise2/src/Main.java
@@ -3,17 +3,10 @@ public class Main {
         if (args.length != 2) {
             return;
         }
-
         int n_teeth = Integer.parseInt(args[0]);
         int max_height = Integer.parseInt(args[1]);
 
-        for (int teeth = 1; teeth <= n_teeth; teeth++) {
-            for (int line = 1; line <= max_height; line++) {
-                for (int star = 1; star <= line; star++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-        }
+        System.out.println("Not yet implemented!");
+
     }
 }
