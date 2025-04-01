9ba6af01aadf73365e637ef04f3547109000b2e9
Maurane Venegas <maurane.venegas@edu.hefr.ch>
2025-04-01 08:32:52 +0200

    Try pipeline with correct code

--- Series5/Exercise2/src/Main.java
+++ Series5/Exercise2/src/Main.java
@@ -3,10 +3,17 @@ public class Main {
         if (args.length != 2) {
             return;
         }
+
         int n_teeth = Integer.parseInt(args[0]);
         int max_height = Integer.parseInt(args[1]);
 
-        System.out.println("Not yet implemented!");
-
+        for (int teeth = 1; teeth <= n_teeth; teeth++) {
+            for (int line = 1; line <= max_height; line++) {
+                for (int star = 1; star <= line; star++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
     }
 }
